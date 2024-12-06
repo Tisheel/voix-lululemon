89c9a46f2cabad292e6f5e463fd996a4a5218703
Tisheel Bashyam <tisheelbashyam@gmail.com>
2024-12-06 16:51:56 +0530

    recoms in cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "../urls/urls";
+import RecommendationsPage from "./Recommendation";
 
 const Cart = () => {
   // const cartItems = [
@@ -25,26 +26,29 @@ const Cart = () => {
   }, []);
 
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-4">Your Cart</h1>
-      <div>
-        {cartItems.map((item) => (
-          <div key={item.id} className="flex justify-between items-center mb-4">
-            <img
-              src={item.image1_url}
-              alt={item.name}
-              className="w-36 h-36 object-cover rounded-lg border"
-            />
-            <p>{item.name}</p>
-            <p>${item.price}</p>
-          </div>
-        ))}
+    <>
+      <div className="container mx-auto py-8">
+        <h1 className="text-3xl font-bold mb-4">Your Cart</h1>
+        <div>
+          {cartItems.map((item) => (
+            <div key={item.id} className="flex justify-between items-center mb-4">
+              <img
+                src={item.image1_url}
+                alt={item.name}
+                className="w-36 h-36 object-cover rounded-lg border"
+              />
+              <p>{item.name}</p>
+              <p>${item.price}</p>
+            </div>
+          ))}
+        </div>
+        {/* <p className="text-xl font-semibold mt-4">Total: ${total.toFixed(2)}</p> */}
+        <button className="mt-6 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+          Checkout
+        </button>
       </div>
-      {/* <p className="text-xl font-semibold mt-4">Total: ${total.toFixed(2)}</p> */}
-      <button className="mt-6 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
-        Checkout
-      </button>
-    </div>
+      <RecommendationsPage />
+    </>
   );
 };
 
